ec3eaaa38a20aa09cf447586dd4eb594af81060e
johwiebe <j.wiebe17@imperial.ac.uk>
2018-07-14 11:18:34 +0100

    i3 config files.

--- install.sh
+++ install.sh
@@ -3,4 +3,4 @@ ln -sv ~/.dotfiles/zshrc ~/.zshrc
 ln -sv ~/.dotfiles/vimrc ~/.vimrc
 ln -sv ~/.dotfiles/gitconfig ~/.gitconfig
 ln -sv ~/.dotfiles/gitignore ~/.gitignore
-
+ln -sv ~/.dotfiles/config/i3 ~/.config/i3_test
