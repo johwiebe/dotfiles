0b69e7cf70682b8017a5f9fa0581660489b10108
johwiebe <j.wiebe17@imperial.ac.uk>
2018-07-14 13:36:12 +0100

    Use stow instead of ln -sv.

--- install.sh
+++ install.sh
@@ -1,6 +1,11 @@
+sudo apt install stow
 # link dotfiles
-ln -sv ~/.dotfiles/zshrc ~/.zshrc
-ln -sv ~/.dotfiles/vimrc ~/.vimrc
-ln -sv ~/.dotfiles/gitconfig ~/.gitconfig
-ln -sv ~/.dotfiles/gitignore ~/.gitignore
-ln -sv ~/.dotfiles/config/i3 ~/.config/i3_test
+# ln -sv ~/.dotfiles/zshrc ~/.zshrc
+# ln -sv ~/.dotfiles/vimrc ~/.vimrc
+stow zsh
+stow vim
+stow git -v
+stow i3
+# ln -sv ~/.dotfiles/gitconfig ~/.gitconfig
+# ln -sv ~/.dotfiles/gitignore ~/.gitignore
+# ln -sv ~/.dotfiles/config/i3 ~/.config/i3_test
