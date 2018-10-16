483e005313bca19c61a2822b85263bee0acc1484
johwiebe <j.wiebe17@imperial.ac.uk>
2018-10-16 15:06:31 +0100

    Specify target dir for stow.

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 # install stow
-sudo apt install stow
+# sudo apt install stow
 
 # link dotfiles
-stow zsh -v
-stow vim -v
-stow git -v
-stow i3 -v
-stow zathura -v
+stow zsh -v     -t /homes/jw3617
+stow vim -v    -t /homes/jw3617
+stow git -v    -t /homes/jw3617
+stow i3 -v    -t /homes/jw3617
+stow zathura -v    -t /homes/jw3617
